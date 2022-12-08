5caa3fc623766af4954f3b4ff6f1a9ba63b7070f
nikokumar <nikolai.kumar@gmail.com>
2022-12-08 10:47:54 -0600

    reuploaded project

---
+++ Demo/utils.py
@@ -0,0 +1,20 @@
+
+# input is an image
+# output is a list of the contours found in the image's edgemap
+def getContours():
+    pass
+
+# input is an image. This uses getContours and fillContours
+# returns a list of masks
+def getMasks():
+    pass
+    
+# input is an image. 
+# returns number of faces detected
+def countFaces():
+    pass
+    
+# input is a list of contours and image dimensions
+# returns a binary image of 1 with 0 on the inside of the contour.
+def fillContours():
+    pass
---
+++ README.md
@@ -0,0 +1,6 @@
+# face_inpainting
+implementation of a neural network that can fill holes in images of faces
+
+# References
+
+[Nvidia Image Inpainting for Irregular Holes Using Partial Convolutions paper](https://arxiv.org/pdf/1804.07723.pdf)
